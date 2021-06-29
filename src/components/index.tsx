906e5baedbeb6943fa8525e7fb133be168d4f117
Eric-Schecter <1090165925@qq.com>
2021-06-29 14:10:33 +0800

    add webpack and reack hooks

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactChild } from 'react';
+import React, { ReactChild, useEffect, useRef } from 'react';
 import { Object3D } from 'three';
 import { LoaderManager } from './loaders';
 
@@ -9,24 +9,31 @@ type Props = {
   normalized?: boolean
 }
 
-const loader = new LoaderManager();
+export const ObjLoader = ({ children, cb = () => { }, className = '', normalized = true }: Props) => {
+  const refLoader = useRef(new LoaderManager());
 
-export const ObjLoader = ({ children, cb = () => { }, className, normalized = true }: Props) => {
   const dragover = (e: React.DragEvent) => {
     e.preventDefault();
   }
 
   const drop = async (e: React.DragEvent) => {
+    if (!refLoader.current) {
+      return;
+    }
     e.stopPropagation();
     e.preventDefault();
     const items = Array.from(e.dataTransfer.items || []);
     if (items.length) {
-      const promises = items.map(item => loader.load(item, normalized))
+      const promises = items.map(item => refLoader.current.load(item, normalized))
       const objs = await Promise.all(promises);
       const parts = objs.flat().flatMap(obj => obj ? [obj] : []);
       cb(parts);
     }
   }
+
+  useEffect(() => {
+    return refLoader.current.dispose;
+  }, [])
 
   return <div
     className={className}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  }
+}
